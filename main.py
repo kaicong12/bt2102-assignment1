e4d0be06189e0f8ab0c96b3dbbe392edb0afa859
mk2905 <marcuskok2000@gmail.com>
2022-03-10 15:58:29 +0800

    update as of 4pm thurs

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from tkinter import Tk
 from sqlalchemy import create_engine
 import pymysql
-
 from landing_page import LandingPage
 from apps.resources.container import Container
-
-
 class LibraryApp(Container):
     def __init__(self):
-        USER = 'kctey'
-        PASSWORD = 'CQu1FxSp'
+        USER = 'root'
+        PASSWORD = 'password'
         HOST = '127.0.0.1'
         PORT = 3306
-        DATABASE = 'Library'
+        DATABASE = 'library'
 
         self.engine = create_engine('mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(
             USER, PASSWORD, HOST, PORT, DATABASE
         ))
-
-engine.connect()   
-print("Success")
+        
+        self.root =Tk()
+        self.landing = LandingPage(self.root, self, self.engine)
+        self.root.mainloop()
+    
+    def return_to_main_menu(self, child):
+        LandingPage(self.root, self, self.engine)
+        child.container.grid_forget()
+app = LibraryApp()
